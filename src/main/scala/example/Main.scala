5b41dd0043d35fcef98bfcb9b632247724c2c960
Tatsunori Tamaki <github@qtamaki.com>
2017-08-25 11:16:25 +0900

    update

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -11,9 +11,12 @@ import scala.concurrent.ExecutionContextExecutorService
 
 object Main {
   def main(args: Array[String]) {
-    defaultEC()
-    pattern1_UnlimitedQueue()
-    pattern2_LimitedQueue()
+    defaultEC
+    pattern1_UnlimitedQueue
+    pattern2_LimitedQueue
+    pattern3_LimitedQueueNarrow
+    pattern4_LimitedQueueNarrow
+    pattern5_LimitedQueueNarrow
   }
   
   def defaultEC() {
@@ -26,18 +29,43 @@ object Main {
     val pool = new ThreadPoolExecutor(1, 30, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable])
     implicit val ec = ExecutionContext.fromExecutorService(pool)
     println("Start pattern 1 use unlimited queue --------------------")
-    task(30)
-    ec.shutdown()
+    callTask
   }
 
   def pattern2_LimitedQueue() {
     val pool = new ThreadPoolExecutor(1, 30, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable](3))
     implicit val ec = ExecutionContext.fromExecutorService(pool)
     println("Start pattern 2 use limited queue --------------------")
-    task(30)
-    ec.shutdown()
+    callTask
   }
 
+  def pattern3_LimitedQueueNarrow() {
+    val pool = new ThreadPoolExecutor(1, 3, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable](3))
+    implicit val ec = ExecutionContext.fromExecutorService(pool)
+    println("Start pattern 3 use limited queue max 3 ---------------")
+    callTask
+  } //ThreadPoolExecutor.CallerRunsPolicy
+
+  def pattern4_LimitedQueueNarrow() {
+    val pool = new ThreadPoolExecutor(1, 3, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable](3), new ThreadPoolExecutor.CallerRunsPolicy)
+    implicit val ec = ExecutionContext.fromExecutorService(pool)
+    println("Start pattern 3 use limited queue max 3 ---------------")
+    callTask
+  }
+
+  def pattern5_LimitedQueueNarrow() {
+    val pool = new ThreadPoolExecutor(1, 3, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable](3), new ThreadPoolExecutor.DiscardPolicy)
+    implicit val ec = ExecutionContext.fromExecutorService(pool)
+    println("Start pattern 3 use limited queue max 3 ---------------")
+    callTask
+  }
+
+  def callTask(implicit ec: ExecutionContextExecutorService) = try {
+     task(30)
+    } finally {
+      ec.shutdown()
+    }
+  
   def task(x: Int)(implicit ec: ExecutionContext) = {
     Range(0, x).map{i=>
       val s = s"Future no.$i "
@@ -46,7 +74,6 @@ object Main {
     }.foreach{f => println("Result: " + Await.result(f, 10.seconds))}
     println("Finish!!!")
   }
-
   
   def proc(str: String, x:Int, sleep: Long): String = {
     Range(0,x).foreach { i => 
